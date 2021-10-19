3d8993bdfc0a861c4b80cff3312afdac8c564125
Ashley <60424761+bluemew0@users.noreply.github.com>
2021-10-18 18:19:44 -0700

    Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,19 @@
+// Summary. Lab 5 - Data Types & Variables
+
+// Description. Declaring and outputting variables for a car
+// @author Ashley Lu
+// @since  2021.10.19
+
+// defining variables
+var make = "Kia";
+var model = "Rio";
+var color = "Silky Silver";
+var year = 2021;
+var age = 2021 - year;
+
+// outputting variables
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
