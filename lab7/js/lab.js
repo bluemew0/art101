e32eb0707bc50142c546bece49adb6d1573cb028
Ashley <60424761+bluemew0@users.noreply.github.com>
2021-10-24 17:03:21 -0700

    Lab 7 Revisions

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -3,25 +3,29 @@
 // Author: Ashley Lu
 // Created:  2021.10.21
 
+//function to shuffle string
+function shuffleName(name) {
+  nameArray = name.toLowerCase().replace(/ /g, "").split("");
+  // shuffle code from https://www.codespeedy.com/shuffle-characters-of-a-string-in-javascript/
+  nameArray.sort(function(){
+    return 0.5-Math.random(0,1);
+  });
+  // making the first letter capitlized
+  nameArray[0] = nameArray[0].toUpperCase();
+  name = nameArray.join("");
+  return name
+};
+
 // function sortName that will take user input to sort back their name
-function sortName() {
-  // declaring userName using window.prompt()
-  var userName = window.prompt("hi! welcome, what is your name?", "my name");
-
-  /* originally used a for loop to iterate through each letter of the name and add to an array
-  var nameArray = [];
-  for (let letter of name) {
-    nameArray.push(letter);
-  };
-  */
-
+function sortName(name) {
   //separating letters, removing whitespace, sorting them, and then joining them into a single string
-  userName = userName.toLowerCase().replace(/ /g, "").split("");
-  var sortedName = userName.sort().join("");
-
+  name = name.toLowerCase().replace(/ /g, "").split("");
+  var sortedName = name.sort().join("");
   return sortedName;
 };
 
 // calling function
-document.writeln("here is your name sorted: <pre>", sortName(), "</pre>");
+var userName = window.prompt("hi! welcome, what is your name?", "my name");
+document.writeln("here is your name sorted: <pre>", sortName(userName), "</pre>");
+document.writeln("here is your name shuffled: <pre>", shuffleName(userName), "</pre>");
 document.writeln("<br>enjoy!");
