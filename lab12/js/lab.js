bf9ca25184eff2e86376ed164d36e6af72e8fb9c
Ashley <60424761+bluemew0@users.noreply.github.com>
2021-11-16 00:11:44 -0800

    Lab 12

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -29,16 +29,16 @@
                 "red": ["#cc444b", "#da5552", "#df7373", "#e39695", "#e4b1ab"],
                 "blue": ["#5e60ce", "#5390d9", "#4ea8de", "#48bfe3", "#56cfe1"],
                 "yellow": ["#f3fd81", "#fffd5d", "#fff15d", "#ffeb5d", "#ffe55d"]
-                }
+              };
   var results = $("<p>")
   var errorMsg = $("<p>");
 
   // variables for creating a color palette
-  var colorsDiv = $("<div id='colors'><div>");
-  var colorsArr = [];
+  var colorsDiv = $("<div id='colors'></div>");
+  var colorsArr = []; // to keep track of the squares in colorsDiv w/o needing 5 IDs
   var hexDiv = $("<div>");
 
-  // appending
+  // appending child to parent
   $("#output").append(results)
   $("#output").append(errorMsg);
   $("#output").append(colorsDiv);
@@ -46,7 +46,7 @@
 
   // adding <div> to an array to iterate through later
   for (let x = 0; x < 5; x++) {
-    var square = $("<div class='square'>")  
+    var square = $("<div class='square'>");
     colorsArr.push(square);
   };
 
@@ -56,17 +56,21 @@
     var color = nameColor(name);
     if (color != null) {
         errorMsg.text("");
-        $(results).html("<p>Your name color is " + color + ".</p>");
+        $(results).text("Your name color is " + color + ".");
+        $(results).css("marginTop", "50px");
 
         // changing background colors of all the square <div> to match palette
         for (let x = 0; x < 5; x++) {
             $(colorsArr[x]).css("backgroundColor", (colorsObj[color])[x]);
+            $(colorsArr[x]).html("<br><p>"+(colorsObj[color])[x]+"</p>");
             $(colorsDiv).append(colorsArr[x]);
-        }
+        };
+        $(colorsDiv).css("columnCount", "5");
     }
-    else {
+    else { // if there is no input
         $(results).html("");
         $(colorsDiv).html("");
         errorMsg.text("Please input a name :(")
-    }
+        $(errorMsg).css("marginTop", "50px");
+    };
   });
